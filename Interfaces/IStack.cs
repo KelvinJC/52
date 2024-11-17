a2b1354683767ffa9e407615f9fd488160566afe
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-11-17 13:33:32 +0100

    Create IStack interface

---
+++ Interfaces/IStack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+namespace Cards.Interfaces
+{
+    interface IStack
+    {
+        ArrayList Cards { get; }
+
+        bool Add(ICard card);
+
+        void Show();
+
+        ICard[] Empty();
+    }
+}
