dfbe9791181062918f03160f6b22a09b0f517233
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-11-17 13:26:51 +0100

    Create IDeck interface

---
+++ Interfaces/IDeck.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace Cards.Interfaces
+{
+    interface IDeck
+    {
+        string[]? Suits { get; }
+        string[]? Ranks { get; }
+
+        ICard[] Cards { get; }
+
+        void Face();
+        void Face(ICard[] cards);
+
+        void Shuffle(int shuffleCount);
+        ICard[] Shuffle(ICard[] cards, int shuffleCount);
+
+        ICard Deal();
+
+        ICard DealFirst();
+
+        ICard DealLast();
+
+        ICard DealSuit(string suit);
+
+        ICard DealRank(string rank);
+
+        (ICard[], ICard[], ICard[]?) Split();
+
+        void ReturnCard(ref ICard? card);
+
+        void ReturnCards(ref ICard[] cards);
+    }
+}
+
+
+
+
+
