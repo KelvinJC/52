01011c61cdc271259261d6394024478cb0e29f20
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-11-17 13:41:52 +0100

    Implement Stack object

---
+++ Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Security.Cryptography;
+using Cards.Interfaces;
+
+namespace Cards
+{
+    class GStack : IStack
+    {
+
+        private ArrayList _Cards { get; set; } = new ArrayList();
+
+        public ArrayList Cards { get { return _Cards; } }
+
+        public bool Add(ICard card)
+        {
+            if (_Cards.Count == 0)
+            {
+                _Cards.Add(card);
+                return true;
+            }
+
+            ICard current = (ICard)_Cards[0]!;
+            if (current.Rank == card.Rank || current.Suit == card.Suit)
+            {
+                _Cards.Add(card);
+                _Cards.Reverse();
+                return true;
+            }
+
+            return false;
+        }
+        public void Show()
+        {
+            ICard card = (ICard)_Cards[0]!;
+            card.Face();
+        }
+
+        public ICard[] Empty()
+        {
+            ICard[] cards = (ICard[])_Cards.ToArray(typeof(ICard));
+            _Cards.Clear();
+            return cards;
+        }
+    }
+}
