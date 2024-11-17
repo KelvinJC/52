1946c71d14dcae1062c3580399f8464c67397b3c
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-11-17 13:32:51 +0100

    Create IPlayer interface

---
+++ Interfaces/IPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace Cards.Interfaces
+{
+    interface IPlayer
+    {
+        string Name { get; }
+
+        void GetCard(ICard card);
+        void GetCards(ICard[] cards, CardPosition place = CardPosition.first);
+
+        void DropHand();
+
+        ICard? PlayCard(ICard card);
+
+        ICard? PlayCardByPosition(CardPosition position = CardPosition.last);
+
+        ICard? PlaySameRankCard(string rank);
+
+        ICard? PlaySameSuitCard(string suit);
+
+        ICard? PlayRandomCard();
+
+        ICard? PlayLowestRankCard(Dictionary<string, int> cardRanking);
+
+        ICard? PlayHighestRankCard(Dictionary<string, int> cardRanking);
+
+        ICard? PlayHigherCard(Dictionary<string, int> cardRanking, ICard cardToBeat);
+    }
+}
